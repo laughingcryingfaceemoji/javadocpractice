848c1056b473113e46368499bb6c9ccba1f92f97
laughingcryingfaceemoji <orkolainenjari@gmail.com>
2025-12-13 01:05:48 +0200

    mkk

---
+++ src/SummonScroll.java
@@ -0,0 +1,12 @@
+public class SummonScroll {
+    int hp;
+    int speed;
+
+    public SummonScroll(int hp, int speed){
+        this.hp = hp;
+        this.speed = speed;
+    }
+    public void threat(String threat){
+        System.out.println("poses threat level : " + threat);
+    }
+}
---
+++ src/summons.java
@@ -0,0 +1,6 @@
+public class summons {
+    public static void main(String[] args) {
+        SummonScroll zombie = new SummonScroll(100, 5);
+        System.out.println("Zombie HP: " + zombie.hp);
+    }
+}
